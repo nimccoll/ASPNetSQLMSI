f97c49bd6ea18c5742172511e16c0b3d37deece5
Nicholas McCollum <nimccoll@microsoft.com>
2020-06-25 13:03:52 -0400

    Initial check-in

---
+++ Pubs.Data/Models/Author.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Pubs.Data.Models
+{
+    /// <summary>
+    /// Represents an author
+    /// </summary>
+    [Serializable]
+    [DataContract(IsReference = true)]
+    public class Author
+    {
+        #region Properties
+
+        /// <summary>
+        /// Unique author identifier
+        /// </summary>
+        [DisplayName("Author ID")]
+        [DataMember]
+        public string AuthorID { get; set; }
+        [DisplayName("First Name")]
+        [DataMember]
+        public string FirstName { get; set; }
+        [DisplayName("Last Name")]
+        [DataMember]
+        public string LastName { get; set; }
+        [DataMember]
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0} {1}", FirstName, LastName);
+            }
+            set
+            {
+
+            }
+        }
+        [DisplayName("Phone Number")]
+        [DataMember]
+        public string PhoneNumber { get; set; }
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string State { get; set; }
+        [DisplayName("Zip Code")]
+        [DataMember]
+        public string PostalCode { get; set; }
+        [DisplayName("Has Contract?")]
+        [DataMember]
+        public bool HasContract { get; set; }
+
+        [DataMember]
+        public virtual ICollection<Title> Titles { get; set; }
+
+        /// <summary>
+        /// Year to date sales
+        /// </summary>
+        [DisplayName("YTD Sales")]
+        [DataMember]
+        public int YearToDateSales { get; set; }
+
+        #endregion
+    }
+}
---
+++ Pubs.Data/Models/Title.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Pubs.Data.Models
+{
+    [Serializable]
+    [DataContract(IsReference = true)]
+    public class Title
+    {
+        #region Properties
+
+        [DisplayName("Title ID")]
+        [Required]
+        [DataMember]
+        public string TitleID { get; set; }
+        [DisplayName("Title")]
+        [DataMember]
+        public string BookTitle { get; set; }
+        [DisplayName("Category")]
+        [DataMember]
+        public string Type { get; set; }
+        [DataMember]
+        public virtual Publisher Publisher { get; set; }
+        [DataMember]
+        public decimal Price { get; set; }
+        [DataMember]
+        public decimal Advance { get; set; }
+        [DataMember]
+        public int Royalty { get; set; }
+        [DisplayName("YTD Sales")]
+        [DataMember]
+        public int YearToDateSales { get; set; }
+        [DataMember]
+        public string Notes { get; set; }
+        [DisplayName("Date Published")]
+        [DataMember]
+        public DateTime PublishDate { get; set; }
+        [DataMember]
+        public virtual ICollection<Author> Authors { get; set; }
+        #endregion
+    }
+}
